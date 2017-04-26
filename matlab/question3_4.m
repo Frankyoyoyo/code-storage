cdacb1e9bec0a3e5bde694cb4413a9b44e10d1d2
Franky <yudr1996@outlook.com>
2017-04-26 23:22:29 +0800

    等势面、等势线

--- matlab/question3_4.m
+++ matlab/question3_4.m
@@ -1,69 +1,54 @@
-% a=0.35;   %radius
-% 
-% %电场线：
-% [X,Y,Z]=meshgrid(-0.5:0.04:0.5);
-% r2=X.^2+Y.^2+Z.^2;
-% for k=1:81
-%     phi=pi/40*(k-1);
-%     costh=cos(phi);
-%     sinth=sin(phi);
-%     R3=(r2+a^2-2*a*(X*costh+Y*sinth)).^(3/2);
-%     Ex0(:,:,:,k)=a*Z*costh./R3;
-%     Ey0(:,:,:,k)=a*Z*sinth./R3;
-%     Ez0(:,:,:,k)=a*(a-X*costh-Y*sinth)./R3;
-% end
-% Ex=pi/40*trapz(Ex0,4)
-% Ey=pi/40*trapz(Ey0,4);
-% Ez=pi/40*trapz(Ez0,4);          %三个方向的电场强度
-% 
-% i=2
-% Ex(1,1,i)
+clear all;
+a=0.35; q=1;
+%固定z，平行于xoy平面的等势线
+z=1;
+x=-1:0.04:1;
+y=-1:0.04:1;
+phi=0:pi/200:2*pi;
+[X,Y,Phi]=meshgrid(x,y,phi);
+R=sqrt((X-a*cos(Phi)).^2 + (Y-a*sin(Phi)).^2 + z.^2);
+u0=q./(2*pi*R);
+u=pi/200*trapz(u0,3);
+figure(1)
+subplot(1,2,1)
+contour(u)
 
-% 
-% v=[-0.2,-0.1,0,0.1,0.2];
-% [Vx,Vy,Vz]=meshgrid(v,v,0);
-% plot3(Vx(:),Vy(:),Vz(:),'r*')
-% streamline(X,Y,Z,Ex,Ey,Ez,Vx,Vy,Vz,[0.01,2000]);
-% streamline(X,Y,Z,-Ex,-Ey,-Ez,Vx,Vy,Vz,[0.01,2000]);
-% hold on;
-% axis([-0.5,0.5,-0.5,0.5,-0.5,0.5]);
-% view(-23,26);
-% box on;
-% t=0:pi/100:2*pi;
-% plot(a*exp(i*t),'r-','LineWidth',3);
-% 
-% %等势面：
+%与yoz平行的平面上的等势线
+clear X Y Z x y z
+y=-1:0.01:1;
+z=-1:0.01:1;
+X=0;% you can change the X value
+[Y,Z,T]=meshgrid(y,z,phi);
+R=sqrt((X-a*cos(T)).^2 + (Y-a*sin(T)).^2 + Z.^2);
+u0=q/(2*pi*R);
+u=pi/200*trapz(u0,3);
+subplot(1,2,2)
+contour(y,z,u,[0.5,1,1.5,2,2.5,3,3.5,3.8])
 
-% the=0:pi/20:pi;
-% R=1;
-% y=-1:0.04:1; 
-% z=-1:0.04:1;
-% [Y,Z]=meshgrid(y,z);
-% 
-% for i=1:100
-%     phi=pi/40*(i-1);
-%     costh=cos(phi);
-%     sinth=sin(phi);
-%     du(:,:,i)=R./sqrt((-R*costh).^2+(Y-R*sinth).^2+Z.^2);
-% end
-% U=trapz(du,3)
-% mesh(Y,Z,U)
-% U=peaks;
-% contour(peaks)        %用contour画平面等值线
-% R=peaks;
-% [c,h]=contourf(R);    %给二维图填涂上颜色
-% clabel(c,h);
-% colorbar
-% axis auto
-
-
-%二维电势图
-the=0:pi/20:pi;
-R=1;
-y=-1:0.04:1; 
-z=-1:0.04:1;
-[Y,Z]=meshgrid(y,z);
-% [Y,Z,T]=meshgrid(y,z,the);
-du=R./sqrt((R*cos(T).^2+(Y-R*sin(T).^2))+Z.^2);
-U=pi/2*trapz(du,3);
-mesh(Y,Z,U)
+%等势面
+clear X Y Z Phi u u0
+a=0.35; q=1;
+[X,Y,Z]=meshgrid(-1:0.04:1);
+for k =1:81
+ phi=pi/40*(k-1);
+ R=sqrt((X-a*cos(phi)).^2 + (Y-a*sin(phi)).^2 + Z.^2);
+ R3 = R.^3;
+ u0(:,:,:,k)=q./(2*pi*R);
+end
+u=pi/40*trapz(u0,4);
+figure(2)
+p = patch(isosurface(X,Y,Z,u,1.5));
+isonormals(X,Y,Z,u,p);
+p1 = patch(isosurface(X,Y,Z,u,2));
+isonormals(X,Y,Z,u,p1);
+p2 = patch(isosurface(X,Y,Z,u,3.8));
+isonormals(X,Y,Z,u,p2);
+isosurface(X,Y,Z,u,3);
+set(p, 'FaceColor', 'yellow', 'EdgeColor', 'none');
+set(p1, 'FaceColor', 'red', 'EdgeColor', 'none');
+set(p2, 'FaceColor', 'blue', 'EdgeColor', 'none');
+daspect([1 1 1]);
+view(3)
+axis tight;grid on;
+camlight;lighting phong
+alpha(0.5)
