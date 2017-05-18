d8a62a6d3cea90e49fc13e7f9882379e8f46f209
Frank <yudr1996@outlook.com>
2017-05-18 19:00:01 +0800

    fft

--- matlab/ex4_4.m
+++ matlab/ex4_4.m
@@ -4,15 +4,19 @@ clc
 x=0:0.1:2;
 y=[0,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0,0,0,0,0,0,0,0,0,0];
 cftool
-p=fft(y); % is complex 
-ppy=p.*conj(p);
+
+% fft of y
+p=fft(y);      %为复数
+ppy=p.*conj(p);%乘共轭求模
 subplot(1,3,1)
 plot(x,ppy)
+
 % inverse fft of y
 ip=ifft(y);
 ippy=ip.*conj(ip)
 subplot(1,3,2)
 plot(x,ippy)
+
 %inverse fft of p
 iip=ifft(p);
 iippy=iip.*conj(iip)
